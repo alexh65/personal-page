9ad92c7276c63a2ef16a33dcc3ecce02b3cce2f2
Alex Hoang <nhuhoang1012@gmail.com>
2020-05-29 12:22:14 -0700

    Move my contact info to the bottom

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="header">
     <h1>Alex Hoang</h1>
-    <div class="contact">
-      <font-awesome-icon :icon="['fas', 'envelope']" />
-      <p>nhuhoang1012@gmail.com</p>
-    </div>
-    <div class="contact"> 
-      <font-awesome-icon :icon="['fas', 'mobile-alt']" />
-      <p>(778) 858-2565</p>
-    </div>
-    <div class="social" >
-      <a href="https://www.instagram.com/lexotterly/"><font-awesome-icon :icon="['fab', 'instagram']" size="2x"/></a>
-      <a href="https://github.com/alexh65"><font-awesome-icon :icon="['fab', 'github']" size="2x" /></a>
-      <a href="https://www.linkedin.com/in/nhu-hoang-77b02b19a/"><font-awesome-icon :icon="['fab', 'linkedin']" size="2x" /></a>
+    <div class="content">
+      <div class="contact">
+        <font-awesome-icon :icon="['fas', 'envelope']" />
+        <p>nhuhoang1012@gmail.com</p>
+      </div>
+      <div class="contact"> 
+        <font-awesome-icon :icon="['fas', 'mobile-alt']" />
+        <p>(778)858-2565</p>
+      </div>
+      <div class="social" >
+        <a href="https://www.instagram.com/lexotterly/"><font-awesome-icon :icon="['fab', 'instagram']" size="2x"/></a>
+        <a href="https://github.com/alexh65"><font-awesome-icon :icon="['fab', 'github']" size="2x" /></a>
+        <a href="https://www.linkedin.com/in/nhu-hoang-77b02b19a/"><font-awesome-icon :icon="['fab', 'linkedin']" size="2x" /></a>
+      </div>
     </div>
   </div>
 </template>
@@ -24,6 +26,7 @@
   display: flex;
   flex-direction: column;
   text-align: center;
+  align-items: center;
 }
 p{
   margin: 0;
@@ -36,6 +39,7 @@ p{
 }
 
 h1 {
+  margin-top: 1vw;
   padding: 2vw;
   font-family: Bock;
   font-size: 4.8vw;
@@ -43,14 +47,12 @@ h1 {
   line-height: 4vw;
 }
 
-ul {
-  padding: 5px;
-  text-align: center;
-  font-size: 1.5vw;
-  line-height: 1.5vw;
-  font-weight: bold;
+.content {
+  position: absolute;
+  bottom: 0;
+  margin-bottom:1vw;
+  font-size:1vw;
 }
-
 #header > .social {
   padding-top: 2vw;
   flex: 1;
